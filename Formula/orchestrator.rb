72c9b30463838aebf5c80523a4744eee78636636
jreleaserbot <jreleaser@kordamp.org>
2026-07-26 03:32:54 +0000

    Orchestrator v0.2.0

--- Formula/orchestrator.rb
+++ Formula/orchestrator.rb
@@ -1,25 +1,25 @@
-# Generated with JReleaser 1.25.0 at 2026-07-22T23:00:23.150360542Z
+# Generated with JReleaser 1.25.0 at 2026-07-26T03:32:42.560595521Z
 
 class Orchestrator < Formula
   desc "Local runner for Interesta Orchestrator"
   homepage "https://www.interesta.ai/orchestrator"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.1.0/orchestrator-0.1.0-linux-aarch64.zip"
-    sha256 "4f3f61c6805aafdd78d1142e31f5d5092236c9f1be2335e4d600c15d7d61af4a"
+    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.2.0/orchestrator-0.2.0-linux-aarch64.zip"
+    sha256 "2b85c9a68178b81ae67394d2e490d085d829f5429c96dfa38fe86114d30d0ecc"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.1.0/orchestrator-0.1.0-linux-x86_64.zip"
-    sha256 "97b0fb7cf973acc2508e2c7e2361f6549d1c86bc30bdaa2630679a992cfb591d"
+    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.2.0/orchestrator-0.2.0-linux-x86_64.zip"
+    sha256 "5faea5bd2f5a5bd4da05759a6c52d5611e99bf7bddc2b9b6cf812f8f14680108"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.1.0/orchestrator-0.1.0-macos-aarch64.zip"
-    sha256 "dd02dbd4ec76046858e135183c556548c619c5e224154d7b14508c1f0188ba4a"
+    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.2.0/orchestrator-0.2.0-macos-aarch64.zip"
+    sha256 "e69d46b4d22bcab023c2d4cb5afd6cf40ee1c94e671a3e1f32dcd5a07f0a4248"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.1.0/orchestrator-0.1.0-macos-x86_64.zip"
-    sha256 "5dfa3012f388a5846ca39c42709327ad9f01f49a6a2d8ca60474ee085eb45b75"
+    url "https://github.com/interesta-ai/homebrew-tap/releases/download/v0.2.0/orchestrator-0.2.0-macos-x86_64.zip"
+    sha256 "8b632332f893a75560c4936e8c41f6b80592ab169b36aba204815ab3ec0c64bf"
   end
 
   def install
@@ -28,6 +28,6 @@ class Orchestrator < Formula
 
   test do
     output = shell_output("#{bin}/orchestrator --version")
-    assert_match "0.1.0", output
+    assert_match "0.2.0", output
   end
 end
